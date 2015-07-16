4a7077e73ec1f7c0673cb3b8f359fe11830c6c5d
moroz <arturmorozv@gmail.com>
2015-07-16 18:54:44 +0300

    need canvas svg image

--- js/common.js
+++ js/common.js
@@ -3,16 +3,41 @@ head.ready(function() {
 // tabs
 (function () {
 	var link = $('.js-config-link'),
+		linkBack = $('.js-config-back'),
 		intro = $('.js-config-intro'),
 		container = $('.js-config-in'),
+		containerLoad = $('.js-config-load'),
+		btnSend = $('.js-config-send'),
 		hash = window.location.hash;
-
 	function loadHouse (hash, typeShow) {
 		if (hash) {
-			var house = hash.substr(2,6);
-			container.load('houses/' + house + '.html', function() {
+			var house = hash.substr(2,6),
+				colours = hash.substr(9).split('&');
+			// loade house
+			containerLoad.load('houses/' + house + '.html', function() {
 				configColours();
+				// colours
+				colours.map(function(i, j) {
+					var name = i.substr(0,1),
+						value = i.substr(2);
+					$('.js-colpick-val').each(function () {
+						var _this = $(this),
+							colpickEl = _this.prev(),
+							inputName = _this.attr('name');
+						if (inputName == name) {
+							_this.val(value);
+							var rgbArray = value.split(',');
+							var rgb = {
+								'r': rgbArray[0],
+								'g': rgbArray[1],
+								'b': rgbArray[2]
+							};
+							colpickEl.colpickSetColor(rgb, true);
+						};
+					});
+				});
 			});
+			// show/hide blocks
 			intro.hide();
 			if (typeShow == 'fadeIn') {
 				container.fadeIn();
@@ -23,18 +48,33 @@ head.ready(function() {
 		};
 	}
 	loadHouse(hash, 'show');
-
-	$(window).on('hashchange', function() {
-		var hash = window.location.hash;
-		if (hash == '') {
-			container.hide();
-			intro.show();
-		}
-		else {
-			loadHouse(hash, 'fadeIn');
-		}
+	// go
+	link.on('click', function () {
+		var hash = $(this).attr('href');
+		loadHouse(hash, 'fadeIn');
 	});
+	// back
+	linkBack.on('click', function () {
+		var hash = $(this).attr('href');
+		window.location.hash = hash;
+		container.hide();
+		intro.fadeIn();
+	});
+	// send
+	btnSend.on('click', function () {
+		html2canvas($('#config'), {
+		    onrendered: function(canvas) {
+		        theCanvas = canvas;
+		        document.body.appendChild(canvas);
 
+		        // Convert and download as image 
+		        Canvas2Image.saveAsPNG(canvas); 
+		        $("#ppp").append(canvas);
+		        // Clean up 
+		        //document.body.removeChild(canvas);
+		    }
+		});
+	});
 }());
 
 // color picker
@@ -49,16 +89,11 @@ function configColours () {
 				flat: true,
 				layout: 'rgbhex',
 				submit: 0,
-				color: defaultColor,
 				onChange: function (hsb, hex, rgb, el, bySetColor) {
 					$('.' + svg).css('fill', 'rgb(' + rgb.r + ', ' + rgb.g + ', '+ rgb.b + ')');
-
 					_this.next().val(rgb.r + ',' + rgb.g + ',' + rgb.b);
-
 					var data = '';
-
 					var str = $('.js-config-form').serializeArray();
-
 					str.map(function(i, j) {
 						if (i.value !== '') {
 							data += '&';
@@ -66,20 +101,12 @@ function configColours () {
 							data += '=';
 							data += i.value;
 						};
-						
 					});
-
-					
-					window.location.hash = hash + data;
-
-					console.log(data);
-
-					
-					// 1=value&2=value if value != ''
-					
-					
+					window.location.hash = hash.substr(0,8) + data;
 				}
-			})
+			});
+			var rgb1 = $.colpick.hexToRgb(defaultColor)
+			_this.colpickSetColor(rgb1, true);
 		});
 
 	var accordTitle = $('.js-config-accord-title'),
